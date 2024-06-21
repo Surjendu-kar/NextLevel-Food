e8deaa6e128fd29fded912d09d922cb5e88a80cf
Surjendu kar <rahulgotp9988@gmail.com>
2024-06-21 14:18:13 +0530

    add meals details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,42 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMealDetails } from "@/lib/meals";
+
 export default function Allmeals({ params }) {
+  const mealsDetails = getMealDetails(params.slug);
+  mealsDetails.instructions = mealsDetails.instructions.replace(
+    /\n/g,
+    "<br />"
+  );
   return (
-    <main>
-      <h1>{params.slug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image
+            src={`/images/${mealsDetails.image}`}
+            alt={mealsDetails.title}
+            fill
+          ></Image>
+        </div>
+        <div className={classes.headerText}>
+          <h1>{mealsDetails.title}</h1>
+          <p className={classes.creator}>
+            by{" "}
+            <a href={`mailto:${mealsDetails.creator_email}`}>
+              {mealsDetails.creator}
+            </a>
+          </p>
+          <p className={classes.summary}>{mealsDetails.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: mealsDetails.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
