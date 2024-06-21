475a2ca2963a3b765386f86291402b53a768cb1b
Surjendu kar <rahulgotp9988@gmail.com>
2024-06-21 12:55:44 +0530

    handle error

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+function Error() {
+  return (
+    <main className="error">
+      <h1>An error Occurred!</h1>
+      <p>Failed to fetch meals data. Please try again later</p>
+    </main>
+  );
+}
+
+export default Error;
--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,4 +1,3 @@
-import React from "react";
 import classes from "./loading.module.css";
 function MealsLoadingPage() {
   return <p className={classes.loading}>Fetching meals...</p>;
