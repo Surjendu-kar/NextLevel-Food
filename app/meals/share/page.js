f75814e174ee4db92c97d8aee26972249a3a014a
Surjendu kar <rahulgotp9988@gmail.com>
2024-06-21 15:11:03 +0530

    ADD : Share form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,53 @@
-export default function Community() {
+import { ImagePicker } from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+
+export default function ShareMealPage() {
   return (
-    <main>
-      <h1>Share Meal</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+        {/* action={formAction} */}
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker />
+          {/* {state.message && <p>{state.message}</p>} */}
+          <p className={classes.actions}>
+            {/* <MealsFormSubmit /> */}
+            <button type="submit">Share meal</button>
+          </p>
+        </form>
+      </main>
+    </>
   );
 }
---
+++ components/meals/image-picker.js
@@ -0,0 +1,59 @@
+"use client";
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+  function handleInputChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleInputChange}
+          required
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
