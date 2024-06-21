ca1f551caafa7c1e2d4815a97717c68559921b39
Surjendu kar <rahulgotp9988@gmail.com>
2024-06-21 13:02:11 +0530

    handle not-found path

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+
+function NotFound() {
+  return (
+    <main className="not-found">
+        <h1>Not found</h1>
+        <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  )
+}
+
+export default NotFound
